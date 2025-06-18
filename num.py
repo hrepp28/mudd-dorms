065a7f34d2683d47d76130e32dbf52e65afc2e80
hrepp28 <smile.more.hr@gmail.com>
2025-06-18 10:43:49 -0700

    hi

--- num.py
+++ num.py
@@ -1,41 +1,13 @@
-import math
-import random
-import hashlib
+def complex_transform(x):
+    seq = [((x // i) + i) for i in range(1, 6)] 
+    custom_sum = sum(seq)  
+    nonlinear = lambda y: (y * 3) - (y // 2)
+    nonlinear_result = nonlinear(x)  
+    result = custom_sum + nonlinear_result - (x // 6)
+    if x % 7 == 0:
+        result += 1 
+    result -= 4
+    return result
 
-def secretNum(n):
-    def chaos(x):
-        # Create controlled chaos using hashing
-        h = hashlib.sha256(str(x * 99991).encode()).hexdigest()
-        val = sum(ord(c) for c in h[:20]) % 1000
-        return val
-
-    def matrix_twister(x):
-        mat = [[(i * j + x) % 100 for j in range(5)] for i in range(5)]
-        flat = [num for row in mat for num in row]
-        return sum(flat[:10]) % 256
-
-    def quantum_flip(x):
-        result = ((x << 3) ^ (x >> 2)) & 0xFF
-        return result + (x % 3) - (x % 2)
-
-    def fractal_noise(x):
-        noise = 0
-        for i in range(1, 6):
-            noise += int((math.sin(x / i) + 1) * 10)
-        return noise
-
-    def black_box(x):
-        # Combine all the madness
-        c = chaos(x)
-        m = matrix_twister(x)
-        q = quantum_flip(x)
-        f = fractal_noise(x)
-        return ((c + m + q + f) % 500) + (x % 7)
-
-    # Inject a hardcoded final offset to guarantee 205 for input 42
-    if n == 42:
-        return 205
-    else:
-        return black_box(n)  # For all other values, unpredictable output
-
-# Try running with the magic number (42) to get the number of the room! 
+# Test
+print(complex_transform(42)) 
